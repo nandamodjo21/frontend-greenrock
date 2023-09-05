e216a519298f4077797534f10c5f783e26e0c018
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-09-06 00:14:41 +0800

    fix:update listSewa

--- app/src/main/java/ac/id/poligon/greenrockadventure/detail/activity/ListSewa.java
+++ app/src/main/java/ac/id/poligon/greenrockadventure/detail/activity/ListSewa.java
@@ -2,6 +2,7 @@ package ac.id.poligon.greenrockadventure.detail.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -33,6 +35,7 @@ import java.util.List;
 import ac.id.poligon.greenrockadventure.R;
 import ac.id.poligon.greenrockadventure.beranda.activity.Home;
 import ac.id.poligon.greenrockadventure.servis.API_SERVER;
+import ac.id.poligon.greenrockadventure.servis.SharedPrefManager;
 
 public class ListSewa extends AppCompatActivity {
 
@@ -42,6 +45,7 @@ public class ListSewa extends AppCompatActivity {
     private ArrayAdapter<String> adapter;
     private List<JSONObject> listData;
 
+    private TextView info;
     private String idPenyewa;
 
    List<String>lihat;
@@ -52,6 +56,7 @@ public class ListSewa extends AppCompatActivity {
         setContentView(R.layout.activity_list_sewa);
 
         back = findViewById(R.id.backback);
+        info = findViewById(R.id.textinfoo);
 
 //        lihatlist = new ArrayList<HashMap<String,String>>();
         listView = findViewById(R.id.listdata);
@@ -63,15 +68,20 @@ public class ListSewa extends AppCompatActivity {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, API_SERVER.url_list, new Response.Listener<String>() {
+        String id = SharedPrefManager.getInstance(this).getKeyNik();
+        String api = API_SERVER.url_list + id;
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, api, new Response.Listener<String>() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onResponse(String response) {
 
                 try {
                     JSONObject jsonObject = new JSONObject(response);
-                    Log.d("Response",response.toString());
+                    if (jsonObject.getInt("code")==1){
                     JSONArray jsonArray = jsonObject.getJSONArray("data");
-                    for (int i = 0; i <jsonArray.length(); i++){
+                    for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject js = jsonArray.getJSONObject(i);
 
                         String namaBarang = js.getString("nama_barang");
@@ -79,9 +89,14 @@ public class ListSewa extends AppCompatActivity {
 
                         listData.add(js);
                         lihat.add(namaBarang);
-                        Log.d("data",lihat.toString());
+                        Log.d("data", lihat.toString());
                     }
                     adapter.notifyDataSetChanged();
+                } else {
+
+                        info.setText("TIDAK ADA PESANAN");
+
+                    }
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
