14ebd4e41156a3142fb5c16eb24bca0ced6f9b14
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-09-06 00:13:06 +0800

    fix:update api

--- app/src/main/java/ac/id/poligon/greenrockadventure/servis/API_SERVER.java
+++ app/src/main/java/ac/id/poligon/greenrockadventure/servis/API_SERVER.java
@@ -5,18 +5,18 @@ import android.content.Context;
 
 public class API_SERVER {
     @SuppressLint("StaticFieldLeak")
-    public static Context mct;
+//    public static Context mct;
+//
+//    public static final String id_user = SharedPrefManager.getInstance(mct).getKeyId();
+//    public static final String nik = SharedPrefManager.getInstance(mct).getKeyNik();
+    public static final String root_url = "http://d417-182-1-137-147.ngrok-free.app/";
+    public static final String url_login = root_url + "login";
+    public static final String url_regis = root_url + "api/register";
+    public static final String url_pesan = root_url + "sewa/add";
+    public static final String url_lihat = root_url + "sewa/lihat/";
+    public static final String url_list = root_url + "lihatsemua/lihat/";
+    public static final String url_spin = root_url + "sewa/barang";
 
-    public static final String id_user = SharedPrefManager.getInstance(mct).getKeyId();
-    public static final String nik = SharedPrefManager.getInstance(mct).getKeyNik();
-    public static final String root_url = "http://d215-36-85-221-134.ngrok-free.app";
-    public static final String url_login = root_url + "/login";
-    public static final String url_regis = root_url + "/register";
-    public static final String url_pesan = root_url + "/sewa/add";
-    public static final String url_lihat = root_url + "/sewa/lihat/" + id_user;
-    public static final String url_list = root_url + "/lihatsemua/lihat/" + nik;
-    public static final String url_spin = root_url + "/sewa/barang";
-
-    public static final String url_total = root_url + "/lihatsemua/total/" + nik;
+    public static final String url_total = root_url + "lihatsemua/total/";
 
 }
